b0fe373a2f47c5d8de3e2d96fcd5aa32206e4205
mohammad19-meet <mohammad19@meet.mit.edu>
2017-11-20 19:16:19 +0200

    “COMMENT”

---
+++ Lab5/lab5.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, colormode
+import turtle
+colormode(255)
+
+class Square(Turtle):
+	def __init__(self,size):
+		Turtle.__init__(self)
+		self.shapesize(size)
+		self.shape("square")
+	def random_color():
+		square1.random_color()
+		r = random.randint(0,256)
+		g = random.randint(0,256)
+		b = random.randint(0,256)
+		self.color(r,g,b)
+square1=Square(10)
+
+class Rectangle(Turtle):
+	def __init__(self,width, height,):
+		Turtle.__init__(self)
+		self.shapesize(size)
+		self.shape("square")
+
+turtle.mainloop()
