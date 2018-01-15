c3213a7f173791490f19e7e67de193febb9edcad
mohammad19-meet <mohammad19@meet.mit.edu>
2018-01-15 19:21:32 +0200

    todays work

--- project/agario.py
+++ project/agario.py
@@ -2,13 +2,14 @@ import turtle
 import time
 import random
 from ball import *
+import math
 
-turtle.tracer(0)
+#turtle.tracer(0)
 turtle.hideturtle()
 RUNNING=True
 SLEEP=0.0077
-SCREEN_WIDTH=turtle.getcanvas().winfo_width()/2
-SCREEN_HEIGHT=turtle.getcanvas().winfo_height()/2
+SCREEN_WIDTH=int(turtle.getcanvas().winfo_width()/2)
+SCREEN_HEIGHT=int(turtle.getcanvas().winfo_height()/2)
 MY_BALL=Ball(0,0,0,2,100,"red")
 NUMBER_OF_BALLS = 5
 MINIMUM_BALL_RADIUS =10
@@ -17,8 +18,37 @@ MINIMUM_BALL_DX = -5
 MAXIMUM_BALL_DX = 5
 MINIMUM_BALL_DY = -5
 MAXIMUM_BALL_DY = 5
+colors=['red','blue',"green",'yellow','brown','pink','purple','orange']
 BALLS =[]
-#for i in range()
+for i in range(NUMBER_OF_BALLS):
+    new_x=random.randint((-SCREEN_WIDTH+MAXIMUM_BALL_RADIUS),(SCREEN_WIDTH-MAXIMUM_BALL_RADIUS))
+    new_y=random.randint(-SCREEN_HEIGHT+MAXIMUM_BALL_RADIUS , (SCREEN_HEIGHT-MAXIMUM_BALL_RADIUS))
+    new_dx=random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+    while new_dx==0:
+        new_dx = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+
+    new_dy=random.randint(MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
+    while new_dy==0:
+        new_dy=random.randint(MINIMUM_BALL_DY , MAXIMUM_BALL_DY)
+    new_radius=random.randint( MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
+    
+    new_color = random.choice(colors)
+    new_ball=Ball(new_x,new_y,new_dx,new_dy,new_radius,new_color)
+    BALLS.append(new_ball)
+
+def  move_all_balls():
+    for i in BALLS:
+        i.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+move_all_balls()
+
+def collide(ball_a , ball_b):
+    if ball_a==ball_b:
+        return False
+    else:
+        D = math.sqrt(math.pow((ball_a.xcor()-ball_b.xcor()),2) + math.pow((ball_a.ycor()-ball_b.ycor()),2))
+
+
 
 turtle.mainloop()
 
--- project/ball.py
+++ project/ball.py
@@ -1,4 +1,4 @@
-from turtle import Turtle
+from turtle import Turtle, mainloop
 class Ball(Turtle):
 	def __init__(self,x,y,dx,dy,r,color):
 		Turtle.__init__(self)
@@ -7,31 +7,38 @@ class Ball(Turtle):
 		self.dx=dx
 		self.dy=dy
 		self.r=r
-		self.shape="circle"
+		self.shape("circle")
 		self.shapesize(r/10)
-		self.color(color, color)
+		self.color(color)
+
 	def move(self,screen_width,screen_height):
 		current_x=self.xcor()
-		new_x=current_x+dx
+		new_x=current_x+self.dx
 		current_y=self.xcor()
-		new_y=current_y+dy
+		new_y=current_y+self.dy
 
 		#qualities to check
-		right_side_ball=new_x+r
-		left_side_ball=new_x-r
-		up_side_ball=new_y+r
-		down_side_ball=new_x-r
+		right_side_ball=new_x+self.r
+		left_side_ball=new_x-self.r
+		up_side_ball=new_y+self.r
+		down_side_ball=new_x-self.r
 		#above is for you to check
 		
-		new_dx =dx
-		new_dy = dy
+		new_dx =self.dx
+		new_dy = self.dy
 
 		self.goto(new_x,new_y)
 		if (right_side_ball>=screen_width):
-			new_dx= -dx
+			new_dx= -self.dx
 		if (left_side_ball<=0):
-			new_dx= -dx
+			new_dx= -self.dx
 		if (up_side_ball>=screen_height):
-			new_dy= -dy
+			new_dy= -self.dy
 		if (down_side_ball<=0):
-			new_dy= -dy
+			new_dy= -self.dy
+
+
+#ball = Ball(50,50,5,0,80,"blue")
+#ball.move(10,10)
+#mainloop()
+#example
